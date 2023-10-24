68179af0d92284cabccb1872e06676c82c460452
3B032104 <3b032104@gm.student.ncut.edu.tw>
2023-10-24 12:39:29 +0800

    Add helloController and folder hello index.blade

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use  App\Http\Controllers\HelloController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,3 +26,5 @@ Route::get('home', function (){
 });
 */
 Route::get('home', [HomeController::class, 'index'])->name('home.index');
+
+Route::get('hello/{name?}', [HelloController::class, 'index'])->name('hello.index');
